957784eb367f5fd9d4bb2b3f41dd77342fa912f6
root <root@raspberrypi.(none)>
2014-06-14 05:08:08 +0700

    preserve anything before major changes

--- web/server.py
+++ web/server.py
@@ -1,30 +1,22 @@
+from datetime import datetime
 import sqlite3
+import re
 from flask import g
-from flask import Flask
+from flask import Flask, request, flash, url_for, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config.from_pyfile('app.cfg')
+db = SQLAlchemy(app)
 
-DATABASE = '../SophiaDB'
 
-def get_db():
-  db = getattr(g,'_database',None)
-  if db is None :
-    db = g._database = sqlite3.connect(DATABASE)
-  return db
-  
-@app.teardown_appcontext
-def close_connection(exception):
-  db = getattr(g,'_database',None)
-  if db is not None:
-    db.close()
- 
 @app.route('/')
 def hello_world():
   return 'Hello World'
 
 @app.route('/temp')
 def temp_page():
-  return render_template('template/temperature.html'
+  return render_template('template/temperature.html')
 
 if __name__ == '__main__':
-  app.debug = True
   app.run(host='0.0.0.0')
